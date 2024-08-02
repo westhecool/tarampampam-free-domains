822193d2dde762e2d37dd645b193b52677992aef
westhecool <52859033+westhecool@users.noreply.github.com>
2024-08-02 19:37:24 -0400

    Register domains

---
+++ domains/dv.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A privately run publicly available archive.',
+  domain: '1bt.uk',
+  subdomain: 'dv',
+  owner: {
+    email: 'wesley@westhedev.xyz',
+  },
+  record: {
+    CNAME: 'datavault.cname.westhedev.xyz',
+  },
+  proxy: false
+})
